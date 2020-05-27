7d197ca0e38d7d567bf16c36c3cd5b53d7427201
Kenneth <gilmorekenneth@gmail.com>
2020-05-27 14:48:06 -0400

    Implemented basic functionality

--- src/index.js
+++ src/index.js
@@ -1,17 +1,116 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: "",
+    };
+    this.handleInput = this.handleInput.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  handleInput(e) {
+    this.setState({ input: e.target.value });
+  }
+
+  handleClick(e) {
+    e.preventDefault();
+    this.props.sendInput(this.state.input);
+  }
+
+  render() {
+    return (
+      <form>
+        <label htmlFor="searchInput">Enter A City</label>
+        <input type="text" id="searchInput" onChange={this.handleInput}></input>
+        <button onClick={this.handleClick}>Enter</button>
+      </form>
+    );
+  }
+}
+
+class TempChunk extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>{this.props.weather.city_name}</h2>
+        <h2>{this.props.weather.country_code}</h2>
+        <h2>{this.props.weather.temp}</h2>
+        <h2>{this.props.innerWeather.description}</h2>
+        <img
+          src={
+            process.env.PUBLIC_URL +
+            `/icons/${this.props.innerWeather.icon}.png`
+          }
+        />
+      </div>
+    );
+  }
+}
+
+// class WindChunk extends React.Component {
+//   constructor(props) {
+//     super(props);
+//   }
+
+//   render() {}
+// }
+
+// class PrecipChunk extends React.Component {
+//   constructor(props) {
+//     super(props);
+//   }
+
+//   render() {}
+// }
+
+class WeatherApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      weather: "",
+      innerWeather: "",
+    };
+    this.handleInput = this.handleInput.bind(this);
+    this.getWeather = this.getWeather.bind(this);
+  }
+
+  async getWeather(city) {
+    const response = await fetch(
+      `https://api.weatherbit.io/v2.0/current?city=${city}&key=c7cefb476e8e46a5a25347062fe6cdde`,
+      { mode: "cors" }
+    );
+    const weather = await response.json();
+    this.setState(() => ({
+      weather: weather.data[0],
+      innerWeather: weather.data[0].weather,
+    }));
+    console.log(this.state.weather);
+  }
+
+  handleInput(input) {
+    this.getWeather(input);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Super Weather App!</h1>
+        <SearchBar sendInput={this.handleInput} />
+        {this.state.weather !== null ? (
+          <div>
+            <TempChunk
+              weather={this.state.weather}
+              innerWeather={this.state.innerWeather}
+            />
+          </div>
+        ) : null}
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<WeatherApp />, document.getElementById("root"));
