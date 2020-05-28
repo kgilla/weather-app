08565b17a55a9fbc55dc8bd887f3e1a09dacc7b1
Kenneth <gilmorekenneth@gmail.com>
2020-05-28 17:43:36 -0400

    Fleshed out functionality and is working pretty well

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./reset.css";
 import "./index.css";
 import { countries } from "./countries.js";
+
+function Header() {
+  return (
+    <header id="top-banner">
+      <h1>4-Cast</h1>
+    </header>
+  );
+}
 
 class SearchBar extends React.Component {
   constructor(props) {
@@ -24,9 +33,14 @@ class SearchBar extends React.Component {
 
   render() {
     return (
-      <form>
-        <label htmlFor="searchInput">Enter A City</label>
-        <input type="text" id="searchInput" onChange={this.handleInput}></input>
+      <form id="search-bar">
+        <label htmlFor="searchInput">Whats The Weather Like In...</label>
+        <input
+          type="text"
+          id="searchInput"
+          placeholder="Enter a city"
+          onChange={this.handleInput}
+        ></input>
         <button onClick={this.handleClick}>Enter</button>
       </form>
     );
@@ -35,36 +49,54 @@ class SearchBar extends React.Component {
 
 function TempChunk(props) {
   return (
-    <div>
-      <h2>{props.weather.city_name}</h2>
-      <h2>{props.country}</h2>
-      <h2>{props.weather.temp}</h2>
+    <div id="temp-chunk">
       <h2>{props.innerWeather.description}</h2>
-      <img
-        src={process.env.PUBLIC_URL + `/icons/${props.innerWeather.icon}.png`}
-        alt="Icon Displaying Weather"
-      />
+      <div id="temp-main">
+        <img
+          src={process.env.PUBLIC_URL + `/icons/${props.innerWeather.icon}.png`}
+          alt="Icon Displaying Weather"
+        />
+        <h2>{Math.round(props.weather.temp)}&deg;C</h2>
+      </div>
+      <h2>Feels Like: {Math.round(props.weather.app_temp)}&deg;C</h2>
     </div>
   );
 }
 
-// class WindChunk extends React.Component {
-//   constructor(props) {
-//     super(props);
-//   }
+function DetailsChunk(props) {
+  return (
+    <div id="details-chunk">
+      <h2>Precipitation: {props.weather.precip} mm/hr</h2>
+      <h2>Cloud Coverage: {props.weather.clouds}%</h2>
+      <h2>Humidity: {props.weather.rh}%</h2>
+      <h2>Wind: {Math.round(props.weather.wind_spd * 3.6)} km/h</h2>
+      <h2>Wind Direction: {props.weather.wind_cdir_full}</h2>
+    </div>
+  );
+}
 
-//   render() {}
-// }
+function Weather(props) {
+  return (
+    <div id="weather-card">
+      <header id="weather-header">
+        <h2>
+          {props.weather.city_name}, {props.country}
+        </h2>
+        <h2>{props.weather.datetime.slice(0, -3)}</h2>
+      </header>
+      <div id="weather-details">
+        <TempChunk
+          weather={props.weather}
+          innerWeather={props.innerWeather}
+          country={props.country}
+        />
+        <DetailsChunk weather={props.weather} />
+      </div>
+    </div>
+  );
+}
 
-// class PrecipChunk extends React.Component {
-//   constructor(props) {
-//     super(props);
-//   }
-
-//   render() {}
-// }
-
-class WeatherApp extends React.Component {
+class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -75,6 +107,7 @@ class WeatherApp extends React.Component {
     this.handleInput = this.handleInput.bind(this);
     this.getWeather = this.getWeather.bind(this);
     this.findCountry = this.findCountry.bind(this);
+    this.updateState = this.updateState.bind(this);
   }
 
   findCountry(weather) {
@@ -84,19 +117,21 @@ class WeatherApp extends React.Component {
     return c.country_name;
   }
 
+  updateState(weather) {
+    this.setState(() => ({
+      weather: weather.data[0],
+      innerWeather: weather.data[0].weather,
+      country: this.findCountry(weather),
+    }));
+  }
+
   async getWeather(city) {
     const response = await fetch(
       `https://api.weatherbit.io/v2.0/current?city=${city}&key=c7cefb476e8e46a5a25347062fe6cdde`,
       { mode: "cors" }
     );
     const weather = await response.json();
-
-    this.setState(() => ({
-      weather: weather.data[0],
-      innerWeather: weather.data[0].weather,
-      country: this.findCountry(weather),
-    }));
-
+    this.updateState(weather);
     console.log(this.state.weather);
   }
 
@@ -106,21 +141,19 @@ class WeatherApp extends React.Component {
 
   render() {
     return (
-      <div>
-        <h1>Super Weather App!</h1>
+      <div id="main">
+        <Header />
         <SearchBar sendInput={this.handleInput} />
         {this.state.weather !== "" ? (
-          <div>
-            <TempChunk
-              weather={this.state.weather}
-              innerWeather={this.state.innerWeather}
-              country={this.state.country}
-            />
-          </div>
+          <Weather
+            weather={this.state.weather}
+            innerWeather={this.state.innerWeather}
+            country={this.state.country}
+          />
         ) : null}
       </div>
     );
   }
 }
 
-ReactDOM.render(<WeatherApp />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
