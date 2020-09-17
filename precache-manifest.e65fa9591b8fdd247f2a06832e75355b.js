c7da25645fdf5a8716228799e8eef168c9e973a2
Kenneth <gilmorekenneth@gmail.com>
2020-09-17 18:59:59 -0400

    Updates

---
+++ precache-manifest.e65fa9591b8fdd247f2a06832e75355b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b8d2c50fdbcf6ac7deb3a812c42dd93",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "7d7105a7958b9f0ecc45",
+    "url": "/weather-app/static/css/main.176b8c12.chunk.css"
+  },
+  {
+    "revision": "89991a8c387cf39b4844",
+    "url": "/weather-app/static/js/2.60433607.chunk.js"
+  },
+  {
+    "revision": "5d6bc10bb3236cc9c493a2555073a252",
+    "url": "/weather-app/static/js/2.60433607.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d7105a7958b9f0ecc45",
+    "url": "/weather-app/static/js/main.ad0a8d8a.chunk.js"
+  },
+  {
+    "revision": "d0e2e579b5155f255804",
+    "url": "/weather-app/static/js/runtime-main.e6f382d7.js"
+  },
+  {
+    "revision": "0b1b018beaf701cac389433edaa35691",
+    "url": "/weather-app/static/media/quino-al-JFeOy62yjXk-unsplash.0b1b018b.jpg"
+  }
+]);
